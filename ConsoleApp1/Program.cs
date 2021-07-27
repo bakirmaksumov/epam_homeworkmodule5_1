17e257a126c2d246832fb140503fce69b5796854
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-07-27 19:49:55 +0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Здравствуйте введите текст:");
+            string inputTxt = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(inputTxt))
+            {
+                Console.WriteLine($"Первый символ: {inputTxt.Substring(0, 1)}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите текст");
+            }
+
+        }
+
+       
+    }
+}
